157ec8e0d5ae0a22d2ae3ef766fa195112c59a38
Scherezade <scheretrabajo@gmail.com>
2026-05-09 19:18:31 +0100

    Update actividad2.py

--- actividad2.py
+++ actividad2.py
@@ -1,3 +1,11 @@
 import streamlit as st
+import streamlit.components.v1 as components
 
 st.title("Análisis comparativo de Modelos de Inteligencia Artificial.")
+
+# Leemos el contenido de tu archivo HTML
+with open("index2.html", "r", encoding="utf-8") as f:
+    html_content = f.read()
+
+# Lo inyectamos en la página de Streamlit
+components.html(html_content, height=600)
