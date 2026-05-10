e4048322988fa22aefec25580b36d00d5ee6dcaa
Scherezade <scheretrabajo@gmail.com>
2026-05-10 11:32:42 +0100

    Update actividad9.py

--- actividad9.py
+++ actividad9.py
@@ -1,3 +1,4 @@
 import streamlit as st
 
-st.title("Googkd")
+st.title("Desarrollo y despliegue web de resultados técnicos.")
+st.write("Se llevó a cabo una labor de autoformación en tecnologías de despliegue web y control de versiones. Mediante el uso del framework Streamlit, se desarrolló una interfaz interactiva alojada en GitHub, que consolida y expone de forma dinámica los resultados de las actividades 7 y 8.")
