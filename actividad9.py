f8cfb1d44a704d0e0db7f9bf5228f1ea83e170b3
Scherezade <scheretrabajo@gmail.com>
2026-05-12 11:37:40 +0100

    Update actividad9.py

--- actividad9.py
+++ actividad9.py
@@ -2,3 +2,4 @@ import streamlit as st
 
 st.title("Desarrollo y despliegue web de resultados técnicos.")
 st.write("Se llevó a cabo una labor de autoformación en tecnologías de despliegue web y control de versiones. Mediante el uso del framework Streamlit, se desarrolló una interfaz interactiva alojada en GitHub, que consolida y expone de forma dinámica los resultados de las actividades 7 y 8.")
+st.write("Asimismo, se  ha completado la página con todo el contenido realizado en las prácticas externas")
