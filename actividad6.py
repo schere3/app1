d608dc27fdbfd0e969b54d2afd12d01977828a9b
Scherezade <scheretrabajo@gmail.com>
2026-05-12 18:20:54 +0100

    Update actividad6.py

--- actividad6.py
+++ actividad6.py
@@ -3,3 +3,5 @@ import streamlit as st
 st.title('Formación en Microsoft Power BI')
 st.write("Certificado por Santander Open Academy")
 
+# --- MOSTRAR IMAGEN ---
+st.image("cursoPowerBI.png", caption="Certificado de Power BI")
