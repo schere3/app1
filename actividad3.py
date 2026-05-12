4fcad3753904dfe8fe20bd1f0798d426f69fda58
Scherezade <scheretrabajo@gmail.com>
2026-05-12 18:20:44 +0100

    Update actividad3.py

--- actividad3.py
+++ actividad3.py
@@ -3,20 +3,5 @@ import streamlit as st
 st.title("Microcredencial sobre Introducción al Machine Learning")
 st.write("Certificado por la Universidad de la Laguna")
 
-# --- MOSTRAR IMAGEN ---
-st.image("cursoPowerBI.png", caption="Certificado de Power BI")
 
-# --- MOSTRAR PDF ---
-def display_pdf(file_path):
-    # Leer el archivo PDF y convertirlo a base64
-    with open(file_path, "rb") as f:
-        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-    
-    # Crear el objeto HTML para embeber el PDF
-    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
-    
-    # Renderizar en la app
-    st.markdown(pdf_display, unsafe_allow_html=True)
 
-# Reemplaza "Nombre_de_tu_archivo.pdf" por el nombre real que aparece en tu lista de archivos de GitHub
-display_pdf("Certificado OpenCourse Santander- Power BI.pdf.pdf")
