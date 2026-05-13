1f928656a9f97c22973d3fd78d40555d97a5114e
Scherezade <scheretrabajo@gmail.com>
2026-05-13 09:21:36 +0100

    Update actividad7.py

--- actividad7.py
+++ actividad7.py
@@ -18,3 +18,51 @@ st.info("""
 1. **Backtesting (2023-2024):** Se entrenó el modelo con datos históricos previos para predecir el año 2025 y comparar los resultados con los datos reales, validando la precisión del algoritmo.
 2. **Pruebas con Datos Sintéticos:** Generación de una serie temporal de 10 años para evaluar la estabilidad del modelo a largo plazo y contrastar la coherencia de la predicción 2026.
 """)
+
+
+# --- FUNCIONES DE CARGA DE DATOS ---
+def pagina_principal():
+    st.title("Modelización Predictiva")
+    st.write("Estamos antes la página principal de nuestro estudio")
+    st.write("Usa el menú de la izquierda para navegar entre las páginas.")
+    # Todo el contenido Markdown debe ir entre comillas triples dentro de st.markdown
+    st.write("Usa el menú de la izquierda para navegar entre las páginas.")
+    
+    # Todo el contenido Markdown debe ir entre comillas triples dentro de st.markdown
+    st.markdown("""
+    ## Índice
+    1. [Introducción](#introducción)
+    2. [Requisitos Previos](#requisitos-previos)
+    3. [Instalación y Configuración](#instalación-y-configuración)
+    4. [Ir a Visualización de Datos](otra-pagina.md#requisitos-previos)
+    ---
+    ## Introducción
+    Aquí va tu texto de introducción...
+    
+    ## Requisitos Previos
+    Aquí los requisitos...
+    
+    ## Instalación y Configuración
+    Pasos de instalación...
+    """)
+    
+def visualizar_datos():
+    st.title("Análisis de Robustez")
+    st.write("Carga un archivo CSV para visualizar los datos.")
+    archivo_cargado = st.file_uploader("Elige un archivo CSV", type="csv")
+
+    if archivo_cargado is not None:
+        df = pd.read_csv(archivo_cargado)
+        st.write("Datos del archivo csv:")
+        st.write(df)
+        st.write("Estadísticas descriptivas")
+        st.write(df.describe())
+
+# menu para llamar a la funcion
+st.sidebar.title("Navegación")
+pagina1 = st.sidebar.selectbox("Selecciona una página", ["Página Principal", 
+                                                        "Visualización de Datos"])
+if pagina1 == "Página Principal":
+    pagina_principal()
+elif pagina1 == "Visualización de Datos":
+    visualizar_datos()
